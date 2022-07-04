e9d2e0cc2c343616f4c8fe66558846fd25e3dbaf
Gift-Banda <bandaolga19@gmail.com>
2022-07-04 13:53:06 +0200

    2 files modified

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const form = document.getElementById('form');
 const selectQuality = document.getElementById('quality-select');
 const selectFormat = document.getElementById('format-select');
 const ytLink = document.getElementById('yt-link');
+const hiddenMessage = document.getElementById('hm');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(`${ytLink.value}`);
-});
+  let link = ytLink.value;
+  let quality = selectQuality.value;
+  let format = selectFormat.value;
 
-function sendData() {
-  console.log('submitted');
-}
+  if (link.split('?')[0] !== 'https://www.youtube.com/watch') {
+    hiddenMessage.innerText = 'enter youtube video link';
+  } else {
+    hiddenMessage.style.display = 'none';
+    fetch(`?url=${link}&format=${format}&quality=${quality}`);
+  }
+});
